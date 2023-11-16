734f698d47679e326cb8af87fad1f09558b5eb4c
Matthieu Morin <matthieu.bernard.morin@gmail.com>
2023-11-16 10:46:28 +0100

    basic service-worker

---
+++ src/service-worker/index.js
@@ -0,0 +1,6 @@
+/// <reference types="@sveltejs/kit" />
+/// <reference no-default-lib="true"/>
+/// <reference lib="esnext" />
+/// <reference lib="webworker" />
+
+const sw = /** @type {ServiceWorkerGlobalScope} */ (/** @type {unknown} */ (self));
